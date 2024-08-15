f0875e30da275a4dd2529723de1ca6b5307b4250
Sebastian <sebastian.ahl@tewiz.de>
2024-08-15 21:22:13 +0700

    - Added dictionaries.py

---
+++ src/___useful_stuff/collections/dictionaries.py
@@ -0,0 +1,16 @@
+"Information about Python Dictionaries"
+# List methods: https://www.w3schools.com/python/python_dictionaries_methods.asp
+
+# Overview collections in Python:
+    # List is a collection which is ordered and changeable. Allows duplicate members.
+    # Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+    # Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+# changeable means, set items can be changed
+# dublicates are not allowed
+# dictionaries are order since Python 3.7 --> not sorted
+
+
+# a set has no index [INT]
+# a set can contain different data types
