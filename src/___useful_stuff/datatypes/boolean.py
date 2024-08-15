36c11348756866b92e50f37ff4a21fbfdc876f6d
Sebastian <sebastian.ahl@tewiz.de>
2024-08-15 11:32:15 +0700

    added: - operators - isinstance function - datatype boolean

---
+++ src/___useful_stuff/datatypes/boolean.py
@@ -0,0 +1,19 @@
+"Boolean"
+
+# function bool
+# to evaluate if a value is True or False
+print("some True values")
+print(bool("24asf34"))
+print(bool(53654))
+print(bool(["abc", "def", "ghi"]))
+
+# return False
+# there are not many cases when the return value will be False:
+print("\nsome False values")
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
--- src/___useful_stuff/useful_functions.py
+++ src/___useful_stuff/useful_functions.py
@@ -1,6 +1,6 @@
 "Useful functions"
 #########################
-# Return datatype
+# Return datatype with type()
 
 X = ("abc")
 Y = (3)
@@ -13,5 +13,8 @@ print("Z: ", type(Z))
 # Random numbers
 import random
 
-print(random.randrange(1, 1000))
+print(random.randrange(1, 1000, step=1))
 
+# Check the datatype with isinstance()
+x = 200
+print(isinstance(x, int))
