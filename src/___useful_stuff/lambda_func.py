6b6840f99b5d8e00cdd071c54f3ebccf1cb65802
Sebastian <sebastian.ahl@tewiz.de>
2024-08-18 17:34:38 +0700

    - added lambda_func.py

---
+++ src/___useful_stuff/lambda_func.py
@@ -0,0 +1,10 @@
+"Python lambda function"
+# any number of args but only one expression
+# https://www.w3schools.com/python/python_lambda.asp
+
+# Syntax
+# lambda arguments : expression
+
+X = lambda A : A + 10
+
+print(X(5))
