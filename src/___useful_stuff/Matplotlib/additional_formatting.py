3c9440d8d76c9aa26becd768bda98aa1e35e6db7
Sebastian <sebastian.ahl@tewiz.de>
2024-11-24 09:03:03 +0100

    added new functions to matplotlib examples

---
+++ src/___useful_stuff/Matplotlib/additional_formatting.py
@@ -0,0 +1,36 @@
+"Additional Formatting Options for matplotlib"
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+###################
+# coordinates for example
+xcoor = np.array([0, 2, 6, 12, 30])
+ycoor = np.array([0, 4, 3, 10, 5])
+
+# linewidth
+plt.plot(xcoor, ycoor, linewidth = '10')
+plt.show()
+
+
+# multiple lines
+ycoor1 = np.array([0, 2, 6, 12, 30])
+ycoor2 = np.array([0, 4, 3, 10, 5])
+
+plt.plot(ycoor1)
+plt.plot(ycoor2)
+plt.show()
+
+
+# achsis labels, plot title + position, fonts,
+plt.plot(xcoor, ycoor)
+
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
+
+plt.xlabel("x-achsis-label", fontdict = font2)
+plt.ylabel("y-achsis-label", fontdict = font2)
+
+plt.title("Plot Title", fontdict = font1, loc = "left")
+
+plt.show()
